9d0feefeb58a43905a60552563d536f227e619b9
Nakshatra Gowda <68193688+NakshatraGowda@users.noreply.github.com>
2022-08-05 21:12:12 +0530

    Add files via upload

---
+++ reverse_array.java
@@ -0,0 +1,21 @@
+package hackerrank;
+import java.util.Scanner;
+public class reverse_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int temp;
+		int a[]=new int[100];
+		int n = s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=n-1;i>=0;i--)
+		{
+		System.out.print(a[i]+" ");
+		}
+	}
+
+}
