8deeda3a063147592690446d49303bd9e039b7e9
Nakshatra Gowda <68193688+NakshatraGowda@users.noreply.github.com>
2022-08-06 23:00:40 +0530

    Add files via upload

--- Array_2D.java
+++ Array_2D.java
@@ -1,3 +1,24 @@
+/*Problem statement - 
+ Given a 2D integer array with n rows and m columns. Print the 0th row from input n times, 1st row n-1 times…..(n-1)th row will be printed 1 time.
+ 
+ Sample Input 1:
+	3 3
+	1 2 3
+	4 5 6
+	7 8 9
+	
+  Sample Output 1 :
+	1 2 3
+	1 2 3
+	1 2 3
+	4 5 6
+	4 5 6
+	7 8 9
+ 
+ */
+
+
+
 package hackerrank;
 import java.util.Scanner;
 public class Array_2D {
