7c8aac1bbe6ac695d67a369603e7497ab207c079
Nakshatra Gowda <68193688+NakshatraGowda@users.noreply.github.com>
2022-08-15 21:11:43 +0530

    Add files via upload

---
+++ Staircase.java
@@ -0,0 +1,50 @@
+/*Staircase detail
+
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Sample input
+4
+
+sample output
+   #
+  ##
+ ###
+####
+*/
+
+
+
+package hackerrank;
+import java.util.Scanner;
+public class Staircase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]=new int[100][100];
+		Scanner s = new Scanner(System.in);
+		int i;
+		int j;
+		int n=s.nextInt();
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n-(i+1);j++)
+			{
+				System.out.print(" ");
+			}
+			for(j=n-(i+1);j<n;j++)
+			{
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+
+}
